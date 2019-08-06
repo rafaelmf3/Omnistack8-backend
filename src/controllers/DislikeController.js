798fcc36d0066e5b9fef9717beb13a5d2ae6fcc8
Rafael Marinho <rafael_mf3@hotmail.com>
2019-08-06 16:36:07 -0300

    controllers

---
+++ src/controllers/DislikeController.js
@@ -0,0 +1,40 @@
+const Dev = require("../models/Dev");
+
+module.exports = {
+  async index(req, res) {
+    const { user } = req.headers;
+
+    const loggedDev = await Dev.findById(user);
+
+    const users = await Dev.find({
+      $and: [
+        { _id: { $ne: user } },
+        { _id: { $nin: loggedDev.likes } },
+        { _id: { $nin: loggedDev.dislikes } }
+      ]
+    });
+
+    return res.json(users);
+  },
+
+  async store(req, res) {
+    const { user } = req.headers;
+    const { devId } = req.params;
+
+    const loggedDev = await Dev.findById(user);
+    const targetDev = await Dev.findById(devId);
+
+    if (!targetDev) {
+      return res.status(400).json({ error: "Dev not exists" });
+    }
+
+    if (targetDev.dislikes.includes(loggedDev._id)) {
+    }
+
+    loggedDev.dislikes.push(targetDev._id);
+
+    await loggedDev.save();
+
+    return res.json(loggedDev);
+  }
+};
