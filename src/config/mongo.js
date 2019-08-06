dbc28b3cd8c0b02ce00d3198d79b96cf49113bbf
Rafael Marinho <rafael_mf3@hotmail.com>
2019-08-06 15:42:20 -0300

    first commit

---
+++ src/config/mongo.js
@@ -0,0 +1,23 @@
+let mongoose = require("mongoose");
+
+const server = "127.0.0.1:27018"; // REPLACE WITH YOUR DB SERVER
+const database = "mongoOmni"; // REPLACE WITH YOUR DB NAME
+
+class Database {
+  constructor() {
+    this._connect();
+  }
+
+  _connect() {
+    mongoose
+      .connect(`mongodb://${server}/${database}`, { useNewUrlParser: true })
+      .then(() => {
+        console.log("Database connection successful");
+      })
+      .catch(err => {
+        console.error("Database connection error");
+      });
+  }
+}
+
+module.exports = new Database();
